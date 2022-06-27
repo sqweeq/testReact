7d7ea3ea643cbc338cdd2b0036707eef382b20d1
sqweeq <54276533+sqweeq@users.noreply.github.com>
2022-06-28 00:01:13 +0800

    testing more

--- src/App.js
+++ src/App.js
@@ -2,10 +2,38 @@ import "./styles.css";
 import React, { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  const [peopleData, setPeopleData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=20")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result.results);
+          setIsLoaded(true);
+          setPeopleData(result.results);
+        },
+        (error) => {
+          console.log(error);
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>{error.message} </div>;
+  } else if (isLoaded === false) {
+    return <div>Loading.... </div>;
+  } else
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        {peopleData.map((item, key) => (
+          <p>{item.email}</p>
+        ))}
+      </div>
+    );
 }
